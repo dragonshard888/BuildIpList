092ece8094e1d9c7f0b43b25ca03b73d9436c099
dragonshard888 <dragonshard888@gmail.com>
2016-12-24 00:35:06 +0300

    add test

--- src/main/java/ru/ListIPAddress/Solution.java
+++ src/main/java/ru/ListIPAddress/Solution.java
@@ -5,17 +5,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 
 public class Solution {
-    public  static void  main(String[] args) throws IOException{
+    public  static void  main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String ipStartPosition = reader.readLine();
         String ipEndPosition = reader.readLine();
 
         ArrayList<String> list_ip = new ArrayList<String>();
+
         InetAddress ip_start = InetAddress.getByName(ipStartPosition);
         InetAddress ip_end = InetAddress.getByName(ipEndPosition);
+
 
         int[] ip_int_st = new int[4];
         int[] ip_int_end = new int[4];
@@ -23,5 +26,13 @@ public class Solution {
             ip_int_st[i] = 0xff & (int) ip_start.getAddress()[i];
             ip_int_end[i] = 0xff & (int) ip_end.getAddress()[i];
         }
+        compareIpStructure(ip_int_st,ip_int_end);
+    }
+
+    public static boolean compareIpStructure(int[] arr1, int[] arr2) throws UnknownHostException {
+        String tmp_ip1 = arr1[0] + "." + arr1[1] + "." + arr1[2] + "." + arr1[3];
+        String tmp_ip2 = arr2[0] + "." + arr2[1] + "." + arr2[2] + "." + arr2[3];
+        return InetAddress.getByName(tmp_ip1).equals(InetAddress.getByName(tmp_ip2));
+
     }
 }
--- src/test/java/ru/ListIPAddress/SolutionTest.java
+++ src/test/java/ru/ListIPAddress/SolutionTest.java
@@ -1,6 +1,21 @@
 package ru.ListIPAddress;
 
+import org.junit.Assert;
+import org.junit.Test;
 
+/**
+ * Created by Family on 24.12.2016.
+ */
 public class SolutionTest {
+    @Test
+    public void testCompareIpStructure() throws Exception {
+        Solution solution = new Solution();
+        int[] ip1 = {1,1,1,1};
+        int[] ip2 = {1,1,1,5};
+        int[] ip3 = {1,1,1,5};
+        Assert.assertEquals(false,solution.compareIpStructure(ip1,ip2));
+        Assert.assertEquals(true,solution.compareIpStructure(ip2,ip3));
+
+    }
 
 }
